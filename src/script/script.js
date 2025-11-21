bb8da71c4a0309f9c75d71e8a4bc83f69595ab9b
depstor <depstor90@gmail.com>
2025-11-21 20:56:11 +0300

    btn-disabled-true-false

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,15 @@
 import '/src/style/main.scss';
+
+const form = document.querySelector('form');
+const input = form.querySelector('.form-control');
+const todoLists = document.querySelector('.todo-lists');
+const btn = form.querySelector('.btn-submit');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+});
+
+input.addEventListener('input', () => {
+    input.value ? btn.disabled = false : btn.disabled = true;
+});
+
