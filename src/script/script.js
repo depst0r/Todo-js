9951decc56b6953d84bf4eb3e453359a88a0270b
sergey_panteleenko <depstor@vk.com>
2025-11-24 01:47:44 +0300

    delete-todo

--- src/script/script.js
+++ src/script/script.js
@@ -5,32 +5,32 @@ const input = form.querySelector('.form-control');
 const todoLists = document.querySelector('.todo-lists');
 const btn = form.querySelector('.btn-submit');
 
-let index = 2;
+let index = 0;
 
-let tasks = [
-    { id: 0, text: "Изучить JavaScript", completed: false },
-    { id: 1, text: "Сделать todo list", completed: true }
-];
+let tasks = [];
 
 const renderTodo = () => {
-    todoLists.innerHTML = tasks.map((todo, i) => {
+    todoLists.innerHTML = tasks.map(todo => {
         return `
-        <div class="todo-text d-flex align-items-center justify-content-between p-3 mb-2 bg-white rounded shadow-sm">
+        <div class="todo-text  d-flex align-items-center justify-content-between p-3 mb-2 bg-white rounded shadow-sm" data-id=${todo.id}>
             <span class="task-content fs-5">${todo.text}</span>
             <button class="btn btn-danger btn-sm rounded-circle delete-btn">
                 <i class="bi bi-x-lg remuve-todo"></i>
             </button>
         </div>
+
         `;
     }).join(' ');
 }
 
 form.addEventListener('submit', e => {
     e.preventDefault();
-    tasks.push({
+
+    tasks.unshift({
         id: index++,
         text: input.value,
-        disabled: false
+        disabled: false,
+        date: new Date().getFullYear(),
     });
     form.reset();
     renderTodo();
@@ -38,6 +38,16 @@ form.addEventListener('submit', e => {
 
 input.addEventListener('input', () => {
     input.value ? btn.disabled = false : btn.disabled = true;
+});
+
+
+todoLists.addEventListener('click', (e) => {
+    if (e.target.classList.contains('bi-x-lg')) {
+        const taskElement = e.target.closest('.todo-text');
+        const taskId = parseInt(taskElement.dataset.id);
+        tasks = tasks.filter(task => task.id !== taskId);
+        renderTodo();
+    }
 });
 
 renderTodo()
